8e29aa81c8ce49061c92d195c56aaa3750014888
dinosaurtirex <sneakybeaky18@gmail.com>
2023-03-13 20:56:53 +0300

    Add AI which is playing with player

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ import random as r
 
 
 user_coords = [5, 5]
+ai_coords = [6, 6]
 
-game_size_y = 8
+game_size_y = 10
 game_size_x = 32
+
+money_each_row = 32
+
+tick = 0.1
 
 user_score = 0
 
@@ -53,6 +58,47 @@ class Renderer:
             print()
 
 
+class AIService:
+
+    def find_path_to_object(
+        self, 
+        position_x: int, 
+        position_y: int, 
+    ) -> list[str]:
+        global entities
+
+        positions = []
+
+        if len(entities) > 0:
+            for i in entities:
+                # Get target coordinates
+                try:
+                    target_x = i[0]
+                    target_y = i[1]
+                except Exception as exc:
+                    return  
+                
+                y_go = target_y - position_y
+                x_go = target_x - position_x
+
+            positions.append([y_go, x_go])
+
+            best_position = sorted(positions)[0]
+
+            y_go = best_position[0]
+            x_go = best_position[1]
+
+            if y_go < 0:
+                return ("w")
+            if y_go > 0:
+                return ("s")
+        
+            if x_go < 0:
+                return "a"
+            if x_go > 0:
+                return "d"
+        
+
 class GameService(Renderer):
 
     def __init__(self, *args, **kwargs):
@@ -87,9 +133,11 @@ class GameService(Renderer):
         return objects_coords
     
     def generate_new_entities(self) -> None:
-        for _ in range(5):
-            x = r.randint(0, game_size_x)
-            y = r.randint(0, game_size_y)
+        global money_each_row
+
+        for _ in range(money_each_row):
+            x = r.randint(0, game_size_x-1)
+            y = r.randint(0, game_size_y-1)
             entities.append([x, y])
     
     def entity_intersection_check(self, x, y, i, objects_coords: list[Coordinate]) -> list[Coordinate]:
@@ -115,46 +163,87 @@ class GameService(Renderer):
 
 
 def renderer():
-    global user_coords 
+    global user_coords, ai_coords
     global entities
+    global tick 
+
     while True:
         coords = Coordinate(
             user_coords[0],
             user_coords[1]
         )
+        ai_coord = Coordinate(
+            ai_coords[0],
+            ai_coords[1]
+        )
         r = GameService(game_size_x, game_size_y)
-        r.update_game_frame([coords.get_coords()])
-        time.sleep(0.1)
+        r.update_game_frame([coords.get_coords(), ai_coord.get_coords()])
+        time.sleep(tick)
         os.system("cls")
 
 
-def key_input():
-    global exit_app 
+def ai_controller():
+    global tick, ai_coords
 
     while True:
-        key = Keyboard().getchar_windows()
+        
+        if exit_app == True:
+            exit(0)
+
+        # Get player coordinates 
+        x, y = ai_coords[0], ai_coords[1]
+
+        # Calculate path to target coordinates
+
+        key = AIService().find_path_to_object(
+            x, y
+        )
+
+        # Do action 
+        
         if key == "d":
+            ai_coords[0] = ai_coords[0] + 1
+        if key == "a":
+            ai_coords[0] = ai_coords[0] - 1
+        if key == "s":
+            ai_coords[1] = ai_coords[1] + 1
+        if key == "w":
+            ai_coords[1] = ai_coords[1] - 1
+
+        time.sleep(tick)
+
+
+def key_input():
+    global exit_app, tick 
+
+    while True:
+        user_key_input = Keyboard().getchar_windows()
+
+        if user_key_input == "d":
             final_coord = user_coords[0] + 1
             user_coords[0] = final_coord
-        if key == "a":
-            final_coord = user_coords[0]  - 1
+        if user_key_input == "a":
+            final_coord = user_coords[0] - 1
             user_coords[0] = final_coord
-        if key == "s":
-            final_coord = user_coords[1]  + 1
+        if user_key_input == "s":
+            final_coord = user_coords[1] + 1
             user_coords[1] = final_coord
-        if key == "w":
-            final_coord = user_coords[1]  - 1
+        if user_key_input == "w":
+            final_coord = user_coords[1] - 1
             user_coords[1] = final_coord
-        if key == "q":
+
+        if user_key_input == "q":
             exit_app = True 
             exit(0)
 
+        time.sleep(tick)
 
 
 def main():
     threads = [
         threading.Thread(target=key_input),
-        threading.Thread(target=renderer)
+        threading.Thread(target=renderer),
+        threading.Thread(target=ai_controller)
     ]
     for th in threads:
         th.start()
